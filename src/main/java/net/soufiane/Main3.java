8126ef48af82c320cf7e9c0444a56c21a909f4db
elghariaoui <selghariaoui@gmail.com>
2025-06-08 17:18:30 +0200

    mise à jour Spark

--- src/main/java/net/soufiane/Main3.java
+++ src/main/java/net/soufiane/Main3.java
@@ -5,25 +5,31 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 //
 //@author Soufiane Elghariaoui
 
 /*
 
-application Spark permettant, à partir d’un
+application Spark en mode cluster permettant, à partir d’un
 fichier texte (ventes.txt) en entré, contenant les ventes d’une entreprise dans
 les différentes villes, de déterminer le total des ventes par ville. La structure
 du fichier ventes.txt est de la forme suivante
 date ville produit prix
  */
 public class Main3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         SparkConf conf = new SparkConf().setAppName("Ventes");
         JavaSparkContext sc = new JavaSparkContext(conf);
         //load file ventes.txt to RDD
-        JavaRDD<String> rddLines = sc.textFile("src/main/resources/ventes.txt");
+        Scanner scanner = new Scanner(new File("/opt/bitnami/spark/context.txt"));
+        String file = scanner.nextLine();
+        JavaRDD<String> rddLines = sc.textFile(file);
 
         // Calcul du total des ventes par ville
         JavaRDD<String> rddTotalVentes = rddLines
